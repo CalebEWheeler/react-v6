6e84052b9176a86673cd4e811bb134e88fa69271
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-09-11 13:09:59 -0500

    refactor: pass props into Pet component and extract props data and pass to different elements.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h2", {}, "Raylee"),
-        React.createElement("h3", {}, "Dog"),
-        React.createElement("h3", {}, "Retriever")
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed)
     ])
 }
 
@@ -12,9 +12,9 @@ const App = () => {
         {},
         [
             React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement(Pet, {name: "Vinny", animal: "Dog", breed: "Canaan Dog"}),
+            React.createElement(Pet, {name: "Raylee", animal: "Dog", breed: "Flat Coated Retriever"}),
+            React.createElement(Pet,  {name: "Sadie", animal: "Dog", breed: "Labrador Retriever"}),
 
         ]
     );
