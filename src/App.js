3fa25aa6e69a663dc085537d51df5822f4c82ea1
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-09-16 19:21:24 -0500

    feat: create new SearchParams component that is utilized in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import ReactDOM from 'react-dom';
-import Pet from "./Pet";
+import SearchParams from './SearchParams';
 
 const App = () => {
-    let animal = ["Dog", "Cat"];
+    let animalList = ["Dog", "Cat"];
 
     return (
         <div>
             <h1>Adopt Me!</h1>
-            <Pet name="Vinny" animal={animal[0]} breed="Canaan Dog" />
-            <Pet name="Raylee" animal={animal[0]} breed="Flat Coated Retriever" />
-            <Pet name="Sadie" animal={animal[0]} breed="Labrodor Retriever" />
-            <Pet name="Dylan" animal={animal[1]} breed="Norweigan Forest Cat" />
+            <SearchParams />
         </div>
     );
 }
