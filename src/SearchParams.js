f155e5567ba6765f46b19242e3486963d99f0263
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-09-16 20:06:15 -0500

    feat: create new ANIMALS array, create new setAnimal() hook, create new select menu populated by ANIMALS and utilize setAnimal() to select an animal.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
+
+const ANIMALS = ["bird", "cat", "elephant", "dog", "rabbit"];
 
 const SearchParams = () => {
-  
-  const location = "San Antonio, TX"
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
         <form>
           <label htmlFor="location">
             location
-            <input id="location" value={location} placeholder="Location"/>
+            <input id="location" onChange={(e) => setLocation(e.target.value)} value={location} placeholder="Location"/>
           </label>
+          <label htmlFor="animal">
+            Animal
+            
+            <select 
+              id="animal"
+              value={animal}
+              onChange={e => setAnimal(e.target.value)}
+              onBlur={e => setAnimal(e.target.value)}
+            >
+
+                <option />
+                {
+                  ANIMALS.map(animal => (
+                    <option value={animal} key={animal}>
+                      {animal}
+                    </option>
+                  ))}
+                </select>
+          </label>      
           <button>Submit</button>
         </form>
     </div>
