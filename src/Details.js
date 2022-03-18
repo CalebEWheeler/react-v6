322a01171e9a2fbee3325e500434fa86022b0132
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2022-03-18 10:53:40 -0500

    feat: create new Carousel.js component to build out images for extra photos. Refactor Details.js component to now utilize Carousel.js component.

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import Carousel from './Carousel';
 
 class Details extends Component {
   
@@ -25,14 +26,17 @@ class Details extends Component {
 
     if (this.state.loading) return <h2>loading ...</h2>
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } = this.state;
 
     return (
       <div className="details">
-        <h1>{name}</h1>
-        <h2>{animal} - {breed} - {city}, {state}</h2>
-        <button>Adopt {name}</button>
-        <p>{description}</p>
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{animal} - {breed} - {city}, {state}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
       </div>
     )
   }
